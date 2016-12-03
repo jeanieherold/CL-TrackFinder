3eb69fea91c75db7d303bc6afce2682d26030acb
jeanieherold <jeaniemherold68@gmail.com>
2016-12-03 17:44:23 -0500

    a start

--- js/tracks.js
+++ js/tracks.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
 
+	var fsjsBadges = [
+		'Introducing JavaScript',
+		'JavaScript Numbers',
+		'an imposter'
+	];
+
   var studentUrl = "https://teamtreehouse.com/jeanieherold.json"
 
   $.getJSON(studentUrl, function(results){
-  	console.log(results);
-  	console.log(results.badges);
+  	// console.log(results);
+  	
+
+		for (i = 0; i < results.badges.length; i++) {
+			for (var j = 0; j < fsjsBadges.length; j++) {
+				if (results.badges[i].name === fsjsBadges[j]) {
+					console.log(results.badges[i].name);
+				}
+			}
+		}
+
   });
 
 
