d5f981f9a5c8e7830ca9dcc5297750186cdc2e2b
jeanieherold <jeaniemherold68@gmail.com>
2016-12-20 17:58:26 -0500

    adding styles toward CL colorstyles; checking cnet track works against my own th id - need other ids to check

--- js/csharp.js
+++ js/csharp.js
@@ -61,8 +61,6 @@ $('#student').on('click', function(){
 //get student treehouse url
 var user = $("#student").val();
 var studentUrl = "https://teamtreehouse.com/" + user + ".json";
-console.log(user);
-console.log(studentUrl);
 
 
 $('#submit').on('click', function(event){
@@ -83,20 +81,80 @@ $('#submit').on('click', function(event){
 
 			//removing the whitespace for accuracy
 			// results.badges[i].name = results.badges[i].name.replace(/\s+/g, '');
+			
+			//obtaining an array ALL badges a student has earned in Treehouse
 			completes.push(results.badges[i].name);
 		}
 
-		//compare completed badges to track badges
+		//make an array of badges completed in Code Louisville Track
 		var common = $.grep(completes, function(element) {
 		return $.inArray(element, netBadges ) !== -1;
 		});
 
+		//make an array of badges still needed in Code Louisville Track
+		var notInCommon = $.grep(netBadges, function(element) {
+		return $.inArray(element, completes ) === -1;
+		});
+
 		console.log(common);
+		console.log(common.length);
 
-		console.log(completes.length);
-		console.log(completes);
+		console.log(notInCommon);
+		console.log(notInCommon.length);
 
-  });
+		var $earned = $('#badges-earned');
+
+		for(i = 0; i < common.length; i++) {
+			$earned.innerHTML += '<li>' + common[i] + '</li>';
+		}
+
+		//Make a list of badges earned in one column and badges needed in 2nd column
+		
+		
+		//primary col - open ol tag --------------------------------------------------
+    var earnedListHTML = '<ol class="list-results">';
+
+    //Loop through the array of badges in common with students earned badges and cNet track results
+    $.each(common, function(i, poke) {
+
+        //open the li tag
+        earnedListHTML += '<li class="earned-badge">';
+        //add h3 tag and poke name
+        earnedListHTML += '<h3 class="name">' + common[i] + '</h3>';
+        //close the li tag
+        earnedListHTML += '</li>';
+
+    }); //end .each loop
+
+    //close ul tag
+    earnedListHTML += '</ol>';
+
+    //set html of the #pokemon ul
+    $('#badges-earned').html(earnedListHTML);
+
+
+    //secondary col - open ol tag -------------------------------------------------
+    var needList = '<ol class="list-results">';
+
+    //Loop through the array of needed badges results
+    $.each(notInCommon, function(i, poke) {
+
+        //open the li tag
+        needList += '<li class="need-badge">';
+        //add h3 tag and poke name
+        needList += '<h3 class="name">' + notInCommon[i] + '</h3>';
+        //close the li tag
+        needList += '</li>';
+
+    }); //end .each loop
+
+    //close ul tag
+    needList += '</ol>';
+
+    //set html of the #pokemon ul
+    $('#badges-needed').html(needList);
+
+  }); //end of .json call
 
 }); //end 'Go' click
 
